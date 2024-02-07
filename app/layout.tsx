d594f86263c21ecb5467ec2ecf5b087ccd82df07
Ben11029 <107007336+Ben11029@users.noreply.github.com>
2024-02-07 22:21:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Pingtao's GPT",
+  description: "要相信光！",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "PingtaoGPT",
     statusBarStyle: "default",
   },
 };
